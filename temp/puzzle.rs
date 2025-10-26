ace34913063ce32e2430d45c40db15b9329e24db
Shadman Shahriar <shadman.shahriar@bracu.ac.bd>
2025-10-27 03:18:17 +0600

    solve 2015/2_1: c, py

--- temp/puzzle.rs
+++ temp/puzzle.rs
@@ -1,6 +1,7 @@
 pub fn process(buff: &str) {
-    let count_up = buff.matches('(').count();
-    println!("{}", 2*count_up - buff.len());
+    println!("{buff}");
 }
 
-pub fn solution() {}
+pub fn solution() {
+    println!("complete!")
+}

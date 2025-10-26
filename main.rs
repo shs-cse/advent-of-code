69bf0e5156cb15ba4da52a017eea5b9542ad33d5
Shadman Shahriar <shadman.shahriar@bracu.ac.bd>
2025-10-27 00:54:00 +0600

    move src/main.rs to main.rs for symmetry

--- main.rs
+++ main.rs
@@ -1,1 +1,17 @@
-src/main.rs
+#[path = "temp/puzzle.rs"]
+mod puzzle;
+
+use std::fs::File;
+use std::io::{self, BufReader, prelude::*};
+
+fn main() -> io::Result<()> {
+    let file = File::open("temp/puzzle.txt")?;
+    let reader = BufReader::new(file);
+
+    for buff in reader.lines() {
+        puzzle::process(&buff?);
+    }
+    puzzle::solution();
+
+    Ok(())
+}
